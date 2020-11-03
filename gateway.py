e68acb1adeaec663a52bef0fe3c2b1a368939ea8
MukhlasAdib <adib.rasyidy@gmail.com>
2020-11-03 16:35:02 +0700

    Create gateway.py

---
+++ gateway.py
@@ -0,0 +1,35 @@
+import numpy
+
+class ieq_sim():
+    def gen_data():
+        #Generate simulated data
+        print('Under construction')
+
+    def gen_json():
+        # Generate json formatted data
+        print('Under construction')
+
+class gateway_handle():
+    def __init__():
+        # Class initialization
+        print('Under construction')
+
+    def connect_inet():
+        # Connect to internet handle
+        print('Under construction')
+
+    def connect_gcp():
+        # Connect to GCP
+        print("Under construction")
+
+    def open_ap():
+        # Open WiFi access point
+        print('Under construction')
+
+    def mqtt_broker():
+        # Start MQTT broker service
+        print('Under construction')
+
+    def send_data():
+        # Send data to GCP handle
+        print('Under construction')

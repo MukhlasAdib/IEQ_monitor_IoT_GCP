e3e2431f24fbbe336bdbf883305ad808823a412d
MukhlasAdib <adib.rasyidy@gmail.com>
2020-11-04 14:13:57 +0700

    Added logging to txt file

--- Test1/gateway.py
+++ Test1/gateway.py
@@ -5,11 +5,15 @@ import ssl
 import time
 import jwt
 import paho.mqtt.client as mqtt
+import json
 
 
 ### Common Variables
 jwt_alg = 'RS256'
 ca_certs = 'roots.pem'
+fdate = ''
+num = 1
+fname = ''
 
 ### Variables for GCP connection
 gw_private = 'rsa_private.pem'
@@ -22,6 +26,21 @@ gw_deviceID = 'tugas_scada_tim7_testDev1'
 pub_gcp_topic = f'/devices/{gw_deviceID}/events'
 
 ### Common functions
+def renew_filename():
+    # Create new filename for logging
+    global fdate
+    global num
+    global fname
+    now = datetime.datetime.now()
+    fdate = now.strftime("%Y%m%d")
+    num = 1
+    for file in os.listdir():
+        if file.endswith('_test1_log.txt') and file.startswith(fdate):
+            fnum = file.split('_')[1]
+            num = int(fnum) + 1
+    fname = fdate + '_' + str(num) + '_test1_log.txt'
+renew_filename()
+
 def create_jwt(project_id, private_key_file, algorithm):
     # Create a JWT to establish an MQTT connection.
     token = {
@@ -37,22 +56,63 @@ def error_str(rc):
     # Convert a Paho error to a human readable string.
     return '{}: {}'.format(rc, mqtt.error_string(rc))
 
+def add_log(msg):
+    # Add log history
+    now = datetime.datetime.now()
+    date_time = now.strftime("%d-%m-%Y, %H:%M:%S")
+    logStr = date_time + '\n'
+    logStr = logStr + msg + '\n'
+    print(logStr)
+    fdate_new = now.strftime("%Y%m%d")
+    if fdate_new != fdate:
+        renew_filename()
+    with open(fname, "a") as f:
+        f.write(logStr + '\n')
+
 ### IEQ Data generator
 class ieq_sim():
-    def gen_data(self):
-        #Generate simulated data
-        print('Under construction')
+    def __init__(self):
+        self.temp_sim = {'min':0,'max':100,'delta':10}
+        self.rh_sim = {'min':0,'max':100,'delta':10}
+        self.lux_sim = {'min':0,'max':100,'delta':10}
+        self.co2_sim = {'min':0,'max':100,'delta':10}
+        self.spl_sim = {'min':0,'max':100,'delta':10}
+        self.temp = self.temp_sim['min']
+        self.rh = self.rh_sim['min']
+        self.lux = self.lux_sim['min']
+        self.co2 = self.co2_sim['min']
+        self.spl = self.spl_sim['min']
+
+    def progress(self,param,sim):
+        # Calculate the new IEQ parameter
+        param = param + sim['delta']
+        if param > sim['max']: param = sim['min']
+        return param
+
+    def update(self):
+        # Update all of IEQ parameters
+        self.temp = self.progress(self.temp,self.temp_sim)
+        self.rh = self.progress(self.rh,self.rh_sim)
+        self.lux = self.progress(self.lux,self.lux_sim)
+        self.co2 = self.progress(self.co2,self.co2_sim)
+        self.spl = self.progress(self.spl,self.spl_sim)
 
     def gen_json(self):
-        # Generate json formatted data
-        print('Under construction')
+        ieq_dict = {
+            'temp': self.temp,
+            'rh': self.rh,
+            'lux': self.lux,
+            'co2': self.co2,
+            'spl': self.spl 
+        }
+        jsonStr = json.dumps(ieq_dict)
+        self.update()
+        return jsonStr
 
 ### Handle connection to GCP MQTT
 class gateway_gcp():
     def __init__(self):
         self.isConnect_gcp = False
-        self.logList = []
-        self.maxLog = 50
         # Set MQTT Client
         self.client = mqtt.Client(client_id = f'projects/{project_id}/locations/{gcp_region}/registries/{gw_registyID}/devices/{gw_deviceID}')
         self.client.username_pw_set(
@@ -62,12 +122,12 @@ class gateway_gcp():
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
         self.client.on_publish = self.on_publish
-        self.add_log('Initiated')
+        add_log('Initiated')
 
     def connect(self):
         # Connect to GCP
         self.client.connect(gcp_hostname,gcp_port)
-        self.add_log('Trying to connect to GCP')
+        add_log('Trying to connect to GCP')
         self.client.loop_start()
         return self.wait_connect_gcp()
 
@@ -78,62 +138,56 @@ class gateway_gcp():
             total_time += 1
         if not self.isConnect_gcp:
             logMsg = 'Cannot connect to GCP MQTT' 
-            self.add_log(logMsg)
+            add_log(logMsg)
             return False
         else:
             return True
 
     def send_data(self,data):
         # Send data to GCP handle
+        while not self.isConnect_gcp: self.connect()
         res = self.client.publish(pub_gcp_topic, data, qos=1)
         logMsg = f'Publishing (ID {res.mid}) : \n' + str(data)
-        self.add_log(logMsg)
-        res.wait_for_publish()
+        add_log(logMsg)
 
     def on_connect(self, unused_client, unused_userdata, unused_flags, rc):
         # Function when device connected
         logMsg = 'Connected to GCP MQTT: \n' + error_str(rc) 
         self.isConnect_gcp = True
-        self.add_log(logMsg)
+        add_log(logMsg)
     
     def on_disconnect(self, unused_client, unused_userdata, rc):
         # Function when device disconnected
         self.isConnect_gcp = False
         logMsg = 'Disconnected from GCP MQTT: \n' + error_str(rc)
-        self.add_log(logMsg)
+        add_log(logMsg)
 
     def on_publish(self, unused_client, unused_userdata, mid):
         # Function when receive PUBACK
         logMsg = f'Publish (ID {mid}) successful'
-        self.add_log(logMsg)
-
-    def add_log(self, msg):
-        # Add log history
-        now = datetime.datetime.now()
-        date_time = now.strftime("%d-%m-%Y, %H:%M:%S")
-        logStr = date_time + '\n'
-        logStr = logStr + msg
-        print(logStr + '\n')
-        self.logList.append(logStr)
-        if len(self.logList) > self.maxLog:
-            self.logList.pop(0)
+        add_log(logMsg)
 
     def stop(self):
-        self.add_log('Stopping GCP client')
+        add_log('Stopping GCP client')
         self.client.disconnect()
         self.client.loop_stop()
 
+### Main function
 def main():
     gcp = gateway_gcp()
-    gcp.connect()
+    ieq = ieq_sim()
+    try:        
+        gcp.connect()
+        while True:
+            data = ieq.gen_json()
+            gcp.send_data(data)
+            time.sleep(5)
     
-    i = 0
-    while i <= 20:
-        gcp.send_data('test' + str(i))
-        i = i + 1
-        time.sleep(2)
-    
-    gcp.stop()
+    except:
+        add_log('Program terminated')
+
+    finally:
+        gcp.stop()
 
 if __name__ == '__main__':
     main()

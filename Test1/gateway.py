3c106b787d1f9ba229839d752c99a36eb755ed9c
MukhlasAdib <adib.rasyidy@gmail.com>
2020-11-05 21:05:28 +0700

    Gateway IoT core added

--- Test1/gateway.py
+++ Test1/gateway.py
@@ -13,6 +13,11 @@ ca_certs = 'roots.pem'
 fdate = ''
 num = 1
 fname = ''
+gw_DEVID = 'DEV001'
+GWYID = 'GWY001'
+dev_keyDir = 'device_key/'
+dev_metaDir = 'device_list/'
+attachedDev = {}
 
 ### Variables for GCP connection
 gw_private = 'rsa_private.pem'
@@ -21,11 +26,10 @@ gcp_region = 'asia-east1'
 gcp_hostname = 'mqtt.googleapis.com'
 gcp_port = 8883
 gw_registyID = 'tugas_scada_tim7_testReg'
-gw_deviceID = 'tugas_scada_tim7_testDev1'
-pub_gcp_topic = f'/devices/{gw_deviceID}/events'
+gateway_id = 'tugas_scada_tim7_gwy001'
 
 ### Variables for Local Connection
-local_hostname = '192.168.100.6'
+local_hostname = 'localhost'
 local_port = 1883
 data_topic = 'gw1/pub'
 
@@ -65,7 +69,7 @@ def add_log(msg):
     now = datetime.datetime.now()
     date_time = now.strftime("%d-%m-%Y, %H:%M:%S")
     logStr = date_time + '\n'
-    logStr = logStr + msg + '\n'
+    logStr = logStr + str(msg) + '\n'
     print(logStr)
     fdate_new = now.strftime("%Y%m%d")
     if fdate_new != fdate:
@@ -113,8 +117,8 @@ class ieq_sim():
             'spl': self.spl,
             'date': nowDate,
             'time': nowTime,
-            'devID': 'DEV001',
-            'gwyID': 'GWY001' 
+            'devID': gw_DEVID,
+            'gwyID': GWYID 
         }
         jsonStr = json.dumps(ieq_dict)
         self.update()
@@ -123,12 +127,10 @@ class ieq_sim():
 ### Handle connection to GCP MQTT
 class mqtt_gcp():
     def __init__(self):
+        self.local_handler = None
         self.isConnect = False
         # Set MQTT Client
-        self.client = mqtt.Client(client_id = f'projects/{project_id}/locations/{gcp_region}/registries/{gw_registyID}/devices/{gw_deviceID}')
-        self.client.username_pw_set(
-            username = 'unused',
-            password = create_jwt(project_id,gw_private,jwt_alg))
+        self.client = mqtt.Client(client_id = f'projects/{project_id}/locations/{gcp_region}/registries/{gw_registyID}/devices/{gateway_id}')
         self.client.tls_set(ca_certs=ca_certs, tls_version=ssl.PROTOCOL_TLSv1_2)
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
@@ -137,6 +139,9 @@ class mqtt_gcp():
 
     def connect(self):
         # Connect to GCP
+        self.client.username_pw_set(
+            username = 'unused',
+            password = create_jwt(project_id,gw_private,jwt_alg))
         self.client.connect(gcp_hostname,gcp_port)
         add_log('Trying to connect to GCP')
         self.client.loop_start()
@@ -156,10 +161,54 @@ class mqtt_gcp():
 
     def send_data(self,data):
         # Send data to GCP handle
-        while not self.isConnect: self.connect()
-        res = self.client.publish(pub_gcp_topic, data, qos=1)
-        logMsg = f'Publishing to GCP (ID {res.mid}) : \n' + str(data)
+        jsonData = json.loads(data)
+        if 'devID' in jsonData:
+            dev_num = jsonData['devID']
+            deviceID = self.auth_device(dev_num)
+            if deviceID:
+                while not self.isConnect: self.connect()
+                sendTopic = f'/devices/{deviceID}/events'
+                res = self.client.publish(sendTopic, data, qos=1)
+                logMsg = f'Publishing to GCP (ID {res.mid}) : \n' + str(data)
+                add_log(logMsg)
+            else:
+                add_log('Unknown source\n' + data)
+        else:
+            add_log('Device info is not included\n' + data)
+
+    def auth_device(self,dev_num):
+        # Check whether the local device is known to gateway
+        if dev_num in attachedDev:
+            return attachedDev[dev_num]
+        
+        metaName = f'{dev_num}_meta.txt'
+        if metaName in os.listdir(dev_metaDir):
+            with open(dev_metaDir+metaName,"r") as f:
+                json_meta = json.load(f)
+        else: 
+            add_log('Meta file not found\n' + metaName)
+            return ''
+
+        keyName = f'{dev_num}_rsa_private.pem'
+        if not keyName in os.listdir(dev_keyDir):
+            add_log('Private key not found\n' + keyName)
+            return ''
+        
+        self.req_attachment(json_meta['ID'],dev_keyDir+keyName)
+        attachedDev[dev_num] = json_meta['ID']
+        return attachedDev[dev_num]
+
+    def req_attachment(self,ID, keyFile):
+        # Send vevice attachment request to GCP
+        token = create_jwt(project_id,keyFile,jwt_alg)
+        token = str(token)[2:-1]
+        payload = '{\"authorization\":\"'+ token +'\"}'
+        print(payload)
+        topic = f'/devices/{ID}/attach'
+        res = self.client.publish(topic,payload,1)
+        logMsg = f'Sending attachment request for {ID} with message ID {res.mid}'
         add_log(logMsg)
+        time.sleep(3)
 
     def on_connect(self, unused_client, unused_userdata, unused_flags, rc):
         # Function when device connected
@@ -175,7 +224,7 @@ class mqtt_gcp():
 
     def on_publish(self, unused_client, unused_userdata, mid):
         # Function when receive PUBACK
-        logMsg = f'Publish ro GCP (ID {mid}) successful'
+        logMsg = f'Publish to GCP (ID {mid}) successful'
         add_log(logMsg)
 
     def stop(self):
@@ -186,12 +235,13 @@ class mqtt_gcp():
 ### Handle connection to local MQTT
 class mqtt_local():
     def __init__(self):
+        self.cloud_handler = None
         self.isConnect = False
         # Set MQTT Client
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
-        self.client.on_message = self.on_gw1_pub_msg
+        self.client.on_message = self.on_unknown_msg
         self.client.on_subscribe = self.on_subscribe
         self.client.message_callback_add(data_topic,self.on_gw1_pub_msg)
         add_log('Connection to Local MQTT Initiated')
@@ -225,7 +275,7 @@ class mqtt_local():
     
     def on_subscribe(self,client, unused_userdata, mid, granted_qos):
         # Function when subscription request responded
-        add_log('Respond for subscription request ID ' + str(mid) + '\n QoS ' + str(granted_qos[0]))
+        add_log('Respond for subscription request ID ' + str(mid) + '\nQoS ' + str(granted_qos[0]))
 
     def on_disconnect(self, unused_client, unused_userdata, rc):
         # Function when device disconnected
@@ -238,6 +288,14 @@ class mqtt_local():
         logMsg = "A new publish on local topic " + str(message.topic) + "\n"
         logMsg = logMsg + str(message.payload)
         add_log(logMsg)
+        jsonStr = message.payload[2:-1]
+        self.cloud_handler.send_data(jsonStr)
+
+    def on_unknown_msg(self, unused_client, unused_userdata, message):
+        # Function when a new publish occured in unknown topic
+        logMsg = "An unknown message from topic " + str(message.topic) + "\n"
+        logMsg = logMsg + str(message.payload)
+        add_log(logMsg)
 
     def stop(self):
         add_log('Stopping Local MQTT client')
@@ -248,6 +306,8 @@ class mqtt_local():
 def main():
     gcp = mqtt_gcp()
     loc = mqtt_local()
+    gcp.local_handler = loc
+    loc.cloud_handler = gcp
     ieq = ieq_sim()
     try:
         gcp.connect()
@@ -257,8 +317,9 @@ def main():
             gcp.send_data(data)
             time.sleep(5)
     
-    except:
+    except Exception as er:
         add_log('Program terminated')
+        add_log(er)
 
     finally:
         gcp.stop()
